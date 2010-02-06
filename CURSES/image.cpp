b77739eff2805a34a880c8dc156570a5e5aa92f3
xxicemanx <gleaso22@gmail.com>
2010-02-05 20:05:13 -0800

    centered the bicubic enlarge

--- CURSES/image.cpp
+++ CURSES/image.cpp
@@ -7,6 +7,9 @@ using namespace std;
 
 #include "image.h"
 
+const bool INTERPL = true;	// use interpolation to enlarge
+const bool CUBIC   = true;	// only valid if INTERPL is true
+
 ImageType::ImageType()
 {
 	N = 0;
@@ -145,157 +148,173 @@ int ImageType::meanGray() const
 
 void ImageType::enlargeImage( int s, const ImageType& old )
 {
-	const bool CUBIC = true;
-
-	int minOld, maxOld, diag, index;
-	int *rows = new int[old.M];
-	int *cols = new int[old.N];
-	cubicSpline row, col;
-
-	setImageInfo( old.N * s, old.M * s, old.Q );
-	
-	// find the lesser of M and N
-	if ( old.M > old.N )
+	if ( INTERPL )
 	{
-		minOld = old.N;
-		maxOld = old.M;
+		setImageInfo( old.N * s, old.M * s, old.Q );
+		for ( int i = 0; i < N; i++ )
+			for ( int j = 0; j < M; j++ )
+				pixelValue[i][j] = old.pixelValue[i/s][j/s];
 	}
 	else
 	{
-		minOld = old.M;
-		maxOld = old.N;
-	}
 
-	// counts diagonally
-	for ( diag = 0; diag < minOld; diag++ )
-	{	
-		// fills the column array
-		for ( index = 0; index < old.N; index++ )
-			cols[index] = old.pixelValue[index][diag];
+		int minOld, maxOld, diag, index;
+		int *rows = new int[old.M];
+		int *cols = new int[old.N];
+		cubicSpline row, col;
+		int offR, offC;
 
-		// sets the rows array
-		for ( index = 0; index < old.M; index++ )
-			rows[index] = old.pixelValue[diag][index];
-
-		( CUBIC ? row.createCubic( rows, old.M ) : row.create( rows, old.M ) );
-		( CUBIC ? col.createCubic( cols, old.N ) : col.create( cols, old.N ) );
-
-		for ( int r = 0; r < N; r++ )
+		setImageInfo( old.N * s, old.M * s, old.Q );
+		
+		// find the lesser of M and N
+		if ( old.M > old.N )
 		{
-			if ( CUBIC )
-				pixelValue[r][diag*s] = (int)col.getCubicVal( (double)r/(N-s+1) * 100.0 );
-			else
-				pixelValue[r][diag*s] = (int)col.getVal( (double)r/(N-s+1) * 100.0 );
-
-			if ( pixelValue[r][diag*s] > 255 )
-				pixelValue[r][diag*s] = 255;
-			if ( pixelValue[r][diag*s] < 0 )
-				pixelValue[r][diag*s] = 0;
+			minOld = old.N;
+			maxOld = old.M;
+		}
+		else
+		{
+			minOld = old.M;
+			maxOld = old.N;
 		}
 
-		for ( int c = 0; c < M; c++ )
-		{
-			if ( CUBIC )
-				pixelValue[diag*s][c] = (int)row.getCubicVal( (double)c/(M-s+1) * 100.0 );
-			else
-				pixelValue[diag*s][c] = (int)row.getVal( (double)c/(M-s+1) * 100.0 );
-
-			if ( pixelValue[diag*s][c] > 255 )
-				pixelValue[diag*s][c] = 255;
-			if ( pixelValue[diag*s][c] < 0 )
-				pixelValue[diag*s][c] = 0;
-		}
-	}
-
-	// fills in the longer right/left sides
-	if ( minOld != maxOld && maxOld == old.M )
-		for ( diag = minOld; diag < maxOld; diag++ )
-		{
+		// counts diagonally
+		for ( diag = 0; diag < minOld; diag++ )
+		{	
+			// fills the column array
 			for ( index = 0; index < old.N; index++ )
 				cols[index] = old.pixelValue[index][diag];
 
+			// sets the rows array
+			for ( index = 0; index < old.M; index++ )
+				rows[index] = old.pixelValue[diag][index];
+
+			( CUBIC ? row.createCubic( rows, old.M ) : row.create( rows, old.M ) );
 			( CUBIC ? col.createCubic( cols, old.N ) : col.create( cols, old.N ) );
 
 			for ( int r = 0; r < N; r++ )
 			{
+				offR = r-s/2;
 				if ( CUBIC )
-					pixelValue[r][diag*s] = (int)col.getCubicVal( (double)r/(N-s+1) * 100.0 );
+					pixelValue[r][diag*s+s/2] = (int)col.getCubicVal( (double)offR/(N-s+1) * 100.0 );
 				else
-					pixelValue[r][diag*s] = (int)col.getVal( (double)r/(N-s+1) * 100.0 );
+					pixelValue[r][diag*s+s/2] = (int)col.getVal( (double)offR/(N-s+1) * 100.0 );
 
-				if ( pixelValue[r][diag*s] > 255 )
-					pixelValue[r][diag*s] = 255;
-				if ( pixelValue[r][diag*s] < 0 )
-					pixelValue[r][diag*s] = 0;
-			}	
+				if ( pixelValue[r][diag*s+s/2] > 255 )
+					pixelValue[r][diag*s+s/2] = 255;
+				if ( pixelValue[r][diag*s+s/2] < 0 )
+					pixelValue[r][diag*s+s/2] = 0;
+			}
+
+			for ( int c = 0; c < M; c++ )
+			{
+				offC = c-s/2;
+				if ( CUBIC )
+					pixelValue[diag*s+s/2][c] = (int)row.getCubicVal( (double)offC/(M-s+1) * 100.0 );
+				else
+					pixelValue[diag*s+s/2][c] = (int)row.getVal( (double)offC/(M-s+1) * 100.0 );
+
+				if ( pixelValue[diag*s+s/2][c] > 255 )
+					pixelValue[diag*s+s/2][c] = 255;
+				if ( pixelValue[diag*s+s/2][c] < 0 )
+					pixelValue[diag*s+s/2][c] = 0;
+			}
 		}
-	else if ( minOld != maxOld && maxOld == old.N )
-		for ( diag = minOld; diag < maxOld; diag++ )
+
+		// fills in the longer right/left sides
+		if ( minOld != maxOld && maxOld == old.M )
+			for ( diag = minOld; diag < maxOld; diag++ )
+			{
+				for ( index = 0; index < old.N; index++ )
+					cols[index] = old.pixelValue[index][diag];
+
+				( CUBIC ? col.createCubic( cols, old.N ) : col.create( cols, old.N ) );
+
+				for ( int r = 0; r < N; r++ )
+				{
+					offR = r-s/2;
+					if ( CUBIC )
+						pixelValue[r][diag*s+s/2] = (int)col.getCubicVal( (double)offR/(N-s+1) * 100.0 );
+					else
+						pixelValue[r][diag*s+s/2] = (int)col.getVal( (double)offR/(N-s+1) * 100.0 );
+
+					if ( pixelValue[r][diag*s+s/2] > 255 )
+						pixelValue[r][diag*s+s/2] = 255;
+					if ( pixelValue[r][diag*s+s/2] < 0 )
+						pixelValue[r][diag*s+s/2] = 0;
+				}	
+			}
+		else if ( minOld != maxOld && maxOld == old.N )
+			for ( diag = minOld; diag < maxOld; diag++ )
+			{
+				for ( index = 0; index < old.M; index++ )
+					rows[index] = old.pixelValue[diag][index];
+
+				( CUBIC ? row.createCubic( rows, old.M ) : row.create( rows, old.M ) );
+
+				for ( int c = 0; c < M; c++ )
+				{
+					offC = c-s/2;
+					if ( CUBIC )
+						pixelValue[diag*s+s/2][c] = (int)row.getCubicVal( (double)offC/(M-s+1) * 100.0 );
+					else
+						pixelValue[diag*s+s/2][c] = (int)row.getVal( (double)offC/(M-s+1) * 100.0 );
+
+					if ( pixelValue[diag*s+s/2][c] > 255 )
+						pixelValue[diag*s+s/2][c] = 255;
+					if ( pixelValue[diag*s+s/2][c] < 0 )
+						pixelValue[diag*s+s/2][c] = 0;
+				}
+			}
+
+		for ( diag = 0; diag < N; diag++ )
 		{
 			for ( index = 0; index < old.M; index++ )
-				rows[index] = old.pixelValue[diag][index];
+				rows[index] = pixelValue[diag][index*s+s/2];
 
 			( CUBIC ? row.createCubic( rows, old.M ) : row.create( rows, old.M ) );
 
 			for ( int c = 0; c < M; c++ )
 			{
+				offC = c-s/2;
 				if ( CUBIC )
-					pixelValue[diag*s][c] = (int)row.getCubicVal( (double)c/(M-s+1) * 100.0 );
+					pixelValue[diag][c] = (int)row.getCubicVal( (double)offC/(M-s+1) * 100.0 );
 				else
-					pixelValue[diag*s][c] = (int)row.getVal( (double)c/(M-s+1) * 100.0 );
+					pixelValue[diag][c] = (int)row.getVal( (double)offC/(M-s+1) * 100.0 );
 
-				if ( pixelValue[diag*s][c] > 255 )
-					pixelValue[diag*s][c] = 255;
-				if ( pixelValue[diag*s][c] < 0 )
-					pixelValue[diag*s][c] = 0;
+				if ( pixelValue[diag][c] > 255 )
+					pixelValue[diag][c] = 255;
+				if ( pixelValue[diag][c] < 0 )
+					pixelValue[diag][c] = 0;
 			}
 		}
 
-	for ( diag = 0; diag < N; diag++ )
-	{
-		for ( index = 0; index < old.M; index++ )
-			rows[index] = pixelValue[diag][index*s];
-
-		( CUBIC ? row.createCubic( rows, old.M ) : row.create( rows, old.M ) );
-
-		for ( int c = 0; c < M; c++ )
+		for ( diag = 0; diag < M; diag++ )
 		{
-			if ( CUBIC )
-				pixelValue[diag][c] = (int)row.getCubicVal( (double)c/(M-s+1) * 100.0 );
-			else
-				pixelValue[diag][c] = (int)row.getVal( (double)c/(M-s+1) * 100.0 );
+			for ( index = 0; index < old.N; index++ )
+				cols[index] = pixelValue[index*s+s/2][diag];
 
-			if ( pixelValue[diag][c] > 255 )
-				pixelValue[diag][c] = 255;
-			if ( pixelValue[diag][c] < 0 )
-				pixelValue[diag][c] = 0;
-		}
+			( CUBIC ? col.createCubic( cols, old.N ) : col.create( cols, old.N ) );
+
+			for ( int r = 0; r < N; r++ )
+			{
+				offR = r-s/2;
+				if ( CUBIC )
+					pixelValue[r][diag] += (int)col.getCubicVal( (double)offR/(N-s+1) * 100.0 );
+				else
+					pixelValue[r][diag] += (int)col.getVal( (double)offR/(N-s+1) * 100.0 );
+				pixelValue[r][diag] /= 2;
+
+				if ( pixelValue[r][diag] > 255 )
+					pixelValue[r][diag] = 255;
+				if ( pixelValue[r][diag] < 0 )
+					pixelValue[r][diag] = 0;
+			}
+		} 
+		
+		delete [] rows;
+		delete [] cols;
 	}
-
-	for ( diag = 0; diag < M; diag++ )
-	{
-		for ( index = 0; index < old.N; index++ )
-			cols[index] = pixelValue[index*s][diag];
-
-		( CUBIC ? col.createCubic( cols, old.N ) : col.create( cols, old.N ) );
-
-		for ( int r = 0; r < N; r++ )
-		{
-			if ( CUBIC )
-				pixelValue[r][diag] += (int)col.getCubicVal( (double)r/(N-s+1) * 100.0 );
-			else
-				pixelValue[r][diag] += (int)col.getVal( (double)r/(N-s+1) * 100.0 );
-			pixelValue[r][diag] /= 2;
-
-			if ( pixelValue[r][diag] > 255 )
-				pixelValue[r][diag] = 255;
-			if ( pixelValue[r][diag] < 0 )
-				pixelValue[r][diag] = 0;
-		}
-	}
-	
-	delete [] rows;
-	delete [] cols;
 }
 
 void ImageType::reflectImage( bool flag, const ImageType& old )
