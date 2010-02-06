09f5e10dc8311aeb5d1e0e33af2e1eed4d90ef97
xxicemanx <gleaso22@gmail.com>
2010-02-05 19:16:36 -0800

    modified:   Makefile 	new file:   cubicSpline.cpp 	new file:   cubicSpline.h 	modified:   image.cpp

---
+++ CURSES/cubicSpline.h
@@ -0,0 +1,29 @@
+#include <iostream>
+
+class cubicSpline
+{
+public:	
+	cubicSpline();
+	cubicSpline( const cubicSpline& );
+	cubicSpline( int[], int );
+	~cubicSpline();
+
+	// builds the cubic spline from a list of values
+	void create( int[], int );
+	void createCubic(int[], int);
+	
+	// returns the value of the spline
+	double getVal( double );
+	double getCubicVal( double );
+//private:
+	double *coef_0;
+	double *coef_1;
+	double *a;
+	double *y;
+	int len; // number of sub intervals (points - 1)
+	int len2;	// number of sub intervals for cubic
+};
+
+// solves the matrix equation Ax=b for x given A and b
+void solveTriDiag( double*, double*, double*, double*, double*, int );
+
--- CURSES/image.cpp
+++ CURSES/image.cpp
@@ -1,6 +1,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <iostream>
+#include "cubicSpline.h"
 
 using namespace std;
 
@@ -21,9 +22,7 @@ ImageType::~ImageType()
 	if ( pixelValue != NULL )
 	{
 		for ( i = 0; i < N; i++ )
-			//first delete the rows
 			delete [] pixelValue[i];
-		//then delete the pointer array
 		delete [] pixelValue;
 	}
 }
@@ -146,10 +145,162 @@ int ImageType::meanGray() const
 
 void ImageType::enlargeImage( int s, const ImageType& old )
 {
+	const bool CUBIC = true;
+
+	int minOld, maxOld, diag, index;
+	int *rows = new int[old.M];
+	int *cols = new int[old.N];
+	cubicSpline row, col;
+
+	setImageInfo( old.N * s, old.M * s, old.Q );
+	
+	// find the lesser of M and N
+	if ( old.M > old.N )
+	{
+		minOld = old.N;
+		maxOld = old.M;
+	}
+	else
+	{
+		minOld = old.M;
+		maxOld = old.N;
+	}
+
+	// counts diagonally
+	for ( diag = 0; diag < minOld; diag++ )
+	{	
+		// fills the column array
+		for ( index = 0; index < old.N; index++ )
+			cols[index] = old.pixelValue[index][diag];
+
+		// sets the rows array
+		for ( index = 0; index < old.M; index++ )
+			rows[index] = old.pixelValue[diag][index];
+
+		( CUBIC ? row.createCubic( rows, old.M ) : row.create( rows, old.M ) );
+		( CUBIC ? col.createCubic( cols, old.N ) : col.create( cols, old.N ) );
+
+		for ( int r = 0; r < N; r++ )
+		{
+			if ( CUBIC )
+				pixelValue[r][diag*s] = (int)col.getCubicVal( (double)r/(N-s+1) * 100.0 );
+			else
+				pixelValue[r][diag*s] = (int)col.getVal( (double)r/(N-s+1) * 100.0 );
+
+			if ( pixelValue[r][diag*s] > 255 )
+				pixelValue[r][diag*s] = 255;
+			if ( pixelValue[r][diag*s] < 0 )
+				pixelValue[r][diag*s] = 0;
+		}
+
+		for ( int c = 0; c < M; c++ )
+		{
+			if ( CUBIC )
+				pixelValue[diag*s][c] = (int)row.getCubicVal( (double)c/(M-s+1) * 100.0 );
+			else
+				pixelValue[diag*s][c] = (int)row.getVal( (double)c/(M-s+1) * 100.0 );
+
+			if ( pixelValue[diag*s][c] > 255 )
+				pixelValue[diag*s][c] = 255;
+			if ( pixelValue[diag*s][c] < 0 )
+				pixelValue[diag*s][c] = 0;
+		}
+	}
+
+	// fills in the longer right/left sides
+	if ( minOld != maxOld && maxOld == old.M )
+		for ( diag = minOld; diag < maxOld; diag++ )
+		{
+			for ( index = 0; index < old.N; index++ )
+				cols[index] = old.pixelValue[index][diag];
+
+			( CUBIC ? col.createCubic( cols, old.N ) : col.create( cols, old.N ) );
+
+			for ( int r = 0; r < N; r++ )
+			{
+				if ( CUBIC )
+					pixelValue[r][diag*s] = (int)col.getCubicVal( (double)r/(N-s+1) * 100.0 );
+				else
+					pixelValue[r][diag*s] = (int)col.getVal( (double)r/(N-s+1) * 100.0 );
+
+				if ( pixelValue[r][diag*s] > 255 )
+					pixelValue[r][diag*s] = 255;
+				if ( pixelValue[r][diag*s] < 0 )
+					pixelValue[r][diag*s] = 0;
+			}	
+		}
+	else if ( minOld != maxOld && maxOld == old.N )
+		for ( diag = minOld; diag < maxOld; diag++ )
+		{
+			for ( index = 0; index < old.M; index++ )
+				rows[index] = old.pixelValue[diag][index];
+
+			( CUBIC ? row.createCubic( rows, old.M ) : row.create( rows, old.M ) );
+
+			for ( int c = 0; c < M; c++ )
+			{
+				if ( CUBIC )
+					pixelValue[diag*s][c] = (int)row.getCubicVal( (double)c/(M-s+1) * 100.0 );
+				else
+					pixelValue[diag*s][c] = (int)row.getVal( (double)c/(M-s+1) * 100.0 );
+
+				if ( pixelValue[diag*s][c] > 255 )
+					pixelValue[diag*s][c] = 255;
+				if ( pixelValue[diag*s][c] < 0 )
+					pixelValue[diag*s][c] = 0;
+			}
+		}
+
+	for ( diag = 0; diag < N; diag++ )
+	{
+		for ( index = 0; index < old.M; index++ )
+			rows[index] = pixelValue[diag][index*s];
+
+		( CUBIC ? row.createCubic( rows, old.M ) : row.create( rows, old.M ) );
+
+		for ( int c = 0; c < M; c++ )
+		{
+			if ( CUBIC )
+				pixelValue[diag][c] = (int)row.getCubicVal( (double)c/(M-s+1) * 100.0 );
+			else
+				pixelValue[diag][c] = (int)row.getVal( (double)c/(M-s+1) * 100.0 );
+
+			if ( pixelValue[diag][c] > 255 )
+				pixelValue[diag][c] = 255;
+			if ( pixelValue[diag][c] < 0 )
+				pixelValue[diag][c] = 0;
+		}
+	}
+
+	for ( diag = 0; diag < M; diag++ )
+	{
+		for ( index = 0; index < old.N; index++ )
+			cols[index] = pixelValue[index*s][diag];
+
+		( CUBIC ? col.createCubic( cols, old.N ) : col.create( cols, old.N ) );
+
+		for ( int r = 0; r < N; r++ )
+		{
+			if ( CUBIC )
+				pixelValue[r][diag] += (int)col.getCubicVal( (double)r/(N-s+1) * 100.0 );
+			else
+				pixelValue[r][diag] += (int)col.getVal( (double)r/(N-s+1) * 100.0 );
+			pixelValue[r][diag] /= 2;
+
+			if ( pixelValue[r][diag] > 255 )
+				pixelValue[r][diag] = 255;
+			if ( pixelValue[r][diag] < 0 )
+				pixelValue[r][diag] = 0;
+		}
+	}
+	
+	delete [] rows;
+	delete [] cols;
 }
 
 void ImageType::reflectImage( bool flag, const ImageType& old )
 {
+
 }
 
 ImageType& ImageType::operator- ( const ImageType& rhs )
@@ -159,29 +310,14 @@ ImageType& ImageType::operator- ( const ImageType& rhs )
 
 void ImageType::negateImage()
 {
+
 }
 
 /******************Josiah's functions*********************/
 
 void ImageType::getSubImage( int ULr, int ULc, int LRr, int LRc, const ImageType& old )
 {
-	int width, height;
-	//get x width
-	height = abs(ULr - LRr);
-	
-	//get y height
-	width = abs(ULc - LRc);
 
-	//make a new array for the exact size of the new subimage
-	setImageInfo(height, width, old.Q);
-	
-
-	//copy over the old stuff into the new subimage array
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < M; j++){
-			pixelValue[i][j] = old.pixelValue[ULr + i][ULc + j];
-		}
-	}
 }
 
 void ImageType::shrinkImage( int s, const ImageType& old )
@@ -201,22 +337,6 @@ void ImageType::rotateImage( int theta, const ImageType& old )
 
 ImageType& ImageType::operator+ ( const ImageType& rhs )
 {
-	//this is the value that determines the weight of each image
-	//large a gives more weight to first image
-	//small a gives more weight to second image
-	//a must be 0 >= a <= 1
-	float a = 0.5;
-
-	//the general formula is aI1(r,c)+(1-a)I2(r,c)
-	for(int i = 0; i < N; i++){
-		for(int j = 0; j < M; j++){
-			//I don't know yet if I need to type cast it, I dont think so
-			//(float)pixelValue[i][j]*a + (1.0 - a)*(float)rhs[i][j];
-			//It turns out that c++ is smart enough to type cast for me
-			pixelValue[i][j] = pixelValue[i][j]*a + (1.0 - a)*rhs.pixelValue[i][j];
-		}
-	}
-
 	return *this;	// temp return value
 }
 
